c31b8f044cf4141d63f7a824926033182e1104d3
IsaiahSung9 <2601496@sit.singaporetech.edu.sg>
2026-09-18 15:40:03 +0800

    Get_valid_input function

--- modular_auditor.py
+++ modular_auditor.py
@@ -1,23 +1,15 @@
-inventory=0
-failed_entry=0
-while True:
-    #inputing quantity
-    quantity=input("Enter a stock quantity or type (quit) to leave: ")
-    #check if integer or not
-    if not quantity.isdigit() and not quantity.lower()=="quit":
+def get_valid_input():
+    # Ask user for quantity
+    quantity = input("Enter a stock quantity or type (quit) to leave: ")
+
+    # Check for quit
+    if quantity.lower() == "quit":
+        return "quit"
+
+    # Check if input is an integer
+    if not quantity.isdigit():
         print("ERROR! Please key in an integer")
-        failed_entry+=1
-        continue
-    #quit statement
-    elif quantity.lower()=="quit":
-        break
-     #converts quantity to integer
-    quantity=int(quantity)
-    inventory+=quantity
-    #check if inventory exceeds 500
-    if inventory>500:
-        print("ALERT! U have exceeded 500 units. OVERSTOCK!")
-        break
-#print total units and failed entries
-print("Total units processed: "+str(inventory))
-print("Number of failed/rejected entries: "+str(failed_entry))
+        return None
+
+    # Convert quantity to integer
+    return int(quantity)
