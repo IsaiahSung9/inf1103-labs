f12edd7f02cb17105ffd392373e6726ef8432d65
IsaiahSung9 <2601496@sit.singaporetech.edu.sg>
2026-09-18 15:43:35 +0800

    Main function

--- modular_auditor.py
+++ modular_auditor.py
@@ -30,3 +30,44 @@ def generate_report(total_units, failed_attempts):
     # Print final report
     print("Total units processed: " + str(total_units))
     print("Number of failed/rejected entries: " + str(failed_attempts))
+
+
+
+# Main program
+inventory = 0
+failed_entry = 0
+deliveries = 0
+
+while True:
+
+    quantity = get_valid_input()
+
+    # Quit program
+    if quantity == "quit":
+        break
+
+    # Invalid input
+    if quantity is None:
+        failed_entry += 1
+        continue
+
+    # Process valid delivery
+    inventory = process_delivery(inventory, quantity)
+
+    # Calculate tax for this delivery
+    tax = calculate_tax(quantity)
+    print("Tax for this delivery: $" + str(tax))
+
+    # Count successful delivery
+    deliveries += 1
+
+    # Check if inventory exceeds 500
+    if inventory > 500:
+        print("ALERT! U have exceeded 500 units. OVERSTOCK!")
+        break
+
+
+# Generate final report
+generate_report(inventory, failed_entry)
+
+print("Total Deliveries Processed: " + str(deliveries))
